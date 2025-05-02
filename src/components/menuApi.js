1eafc70f212e5b9cc7cd3283cf690f36b816b2d9
abhishekpnaik05 <abhishekpnaik05@gmail.com>
2025-05-02 15:28:27 +0530

    final commit

---
+++ src/components/menuApi.js
@@ -0,0 +1,119 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "Maggi is a quick and delicious instant noodle dish, ready in just 2 minutes with its signature masala flavor. It's the perfect comfort food for any time of day.",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakoda",
+      category: "evening",
+      price: "20₹",
+      description:
+      "Aloo Pakoda is a crispy Indian snack made by deep-frying spiced potato slices dipped in gram flour batter. It's a perfect tea-time treat, especially during rainy weather.",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+      "Corn is a sweet and juicy snack that can be boiled, roasted, or sautéed with butter and spices. It's a healthy, quick, and tasty option enjoyed by all ages.",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Chola (or Chole) is a spicy and flavorful North Indian dish made from chickpeas cooked in a rich onion-tomato gravy. It's often served with bhature, rice, or roti for a hearty meal.",   
+     },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Pizza is a popular Italian dish made with a crispy baked dough base, topped with tomato sauce, cheese, and a variety of vegetables or meats. It's a universal favorite for parties, snacks, or meals.",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Non-Veg Thali is a hearty Indian platter featuring a variety of meat-based dishes like chicken curry, mutton, fish, along with rice, roti, pickles, and dessert. It offers a complete and flavorful meal experience.",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Sweets are delightful desserts made from ingredients like milk, sugar, and nuts, commonly enjoyed during festivals and celebrations. Popular Indian sweets include gulab jamun, jalebi, rasgulla, and laddoo.",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Rajma Rice is a comforting North Indian dish made with red kidney beans cooked in a spicy tomato gravy, served over steamed rice. It's a wholesome and protein-rich meal loved by all ages.",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Samosa is a crispy, deep-fried pastry filled with a spicy potato and pea mixture. It's a popular Indian snack enjoyed with chutney, especially during tea time.",
+    },
+    {
+        id: 10,
+        image: "../images/Bhelpuri.jpg",
+        name: "Bhelpuri",
+        category: "evening",
+        price: "15₹",
+        description:"Bhel Puri is a tangy and crunchy Indian street food made with puffed rice, sev, vegetables, and chutneys. It's a light and flavorful snack perfect for anytime cravings."
+    },
+    {
+        id: 11,
+        image: "../images/alootikki.jpg",
+        name: "Aloo Tikki",
+        category: "evening",
+        price: "20₹",
+        description:"Aloo Tikki is a crispy and spicy potato patty, often served with chutneys or as part of chaat. It's a popular North Indian street snack loved for its rich flavor and crunchy texture."
+    },
+    {
+        id: 12,
+        image: "../images/Pani-Puri.jpg",
+        name: "Pani Puri",
+        category: "evening",
+        price: "20₹",
+        description:"Pani Puri is a beloved Indian street snack consisting of crispy puris filled with spicy, tangy water, mashed potatoes, and chutneys. It's a burst of flavors in every bite!"
+
+    },
+    {
+        id: 13,
+        image: "../images/masala-vada.png",
+        name: "Masala Vada ",
+        category: "evening",
+        price: "20₹",
+        description:"Masala Vada is a crunchy South Indian fritter made from chana dal, spices, and herbs. It's a savory tea-time snack enjoyed hot with coconut chutney."
+    }
+  ];
+  
+  export default Menu;
